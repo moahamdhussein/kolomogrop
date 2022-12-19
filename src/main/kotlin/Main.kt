13f69d33e38dba06b2adf24105b53525658b9158
moahmed hussein <mh95568@gmail.com>
2022-12-19 04:57:25 +0200

    final commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,28 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.math.RoundingMode
+import kotlin.math.abs
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    println("enter dAlpha ")
+    val d = readln().toDouble()
+    println("how many number you have to test")
+    val n = readln().toInt()
+    val numbers :MutableList<Double> = MutableList(n){0.0}
+    val diff :MutableList<Double> = MutableList(n){0.0}
+    for (i in numbers.indices){
+        println("enter number")
+        numbers[i] = readln().toDouble()
+    }
+    numbers.sort()
+    var i =1
+    while (i <= numbers.size){
+
+        diff[i-1] = abs((i.toDouble()/n)-numbers[i-1]).toBigDecimal().setScale(2,RoundingMode.HALF_EVEN).toDouble()
+        i++
+    }
+    println(diff.max())
+    if (diff.max() < d){
+        println("the hypothesis not rejected")
+    }else{
+        println("the hypothesis is rejected.")
+    }
 }
